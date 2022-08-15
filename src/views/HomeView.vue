afdc542c4f1d8d6b02948ed54965890561751328
fakhradine hind <fakhradinehind@gmail.com>
2022-08-15 12:18:18 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    
+    <navbar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import navbar from '@/components/Home/navbar.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    navbar,
+}
+}
+</script>
