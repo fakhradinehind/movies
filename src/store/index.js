c90eb0e32fe35c2d5edd81c456ee6324aa3c6aee
fakhradine hind <fakhradinehind@gmail.com>
2022-08-16 10:44:50 +0200

    sign in 2 ok

--- src/store/index.js
+++ src/store/index.js
@@ -3,21 +3,40 @@ import axios from 'axios'
 
 export default createStore({
   state: {
+    token:null,
+    user:null
   },
   mutations: {
+    setToken(state,token){
+      state.token=token
+    },
+    setUser(state,data){
+      state.user=data
+    }
   },
   actions: {
     async signIn({ dispatch }, credentials) {
 
         try {
           const response = await axios.post('auth/login', credentials)
-          console.log(response.data)
-
+          
+            dispatch('attemp',response.data.access_token)
         } catch (error) {
           console.log(error)
         }
       },  
-      
+      async attemp({commit},token){
+        try{
+          const response =await axios.get('auth/user-profile',{
+            headers:{'Authorization' :`Bearer ${token}`}
+          })
+            commit('setToken',token)
+            commit('setUser',response.data)
+            console.log('successful')
+        }catch(error){
+            console.log('failed2')
+        }
+      }
   },
   modules: {
     
